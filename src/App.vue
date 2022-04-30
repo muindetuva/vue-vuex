981c00f8a539d10795f52c6730c6caa2ad772b58
Alfred Tuva <muindetuva@gmail.com>
2022-04-30 10:55:31 +0300

    Use getters to get data from the store

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <base-container title="Vuex">
+    <favorite-value></favorite-value>
     <the-counter></the-counter>
     <button @click="addOne">Add 1</button>
     <change-counter></change-counter>
@@ -10,12 +11,14 @@
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
     ChangeCounter,
+    FavoriteValue,
   },
 
   methods: {
