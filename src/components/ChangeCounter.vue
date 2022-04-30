b36fe3b9a71d5e427d7e4e5624d724fee036de61
Alfred Tuva <muindetuva@gmail.com>
2022-04-30 10:23:13 +0300

    Initial commit

---
+++ src/components/ChangeCounter.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="addOne">Add 10</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    addOne() {
+      this.$store.commit('increase', { value: 10 });
+    },
+  },
+};
+</script>
+t
